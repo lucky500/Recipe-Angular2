327b8d7074e442fba69fa12a8ac40f79851d2a1a
Guest User <Guest@epicodus-5.local>
2017-10-24 11:52:00 -0700

    initialize recipe-detail.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,63 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Angular2 Scaffold</h1>
-  `
+  <div class="container">
+    <div class="row">
+      <div class="col s12">
+        <h5>{{title}}</h5>
+        <ul class="recipes">
+          <li *ngFor="let recipe of recipes"
+            [class.selected]="recipe === selectedRecipe"
+            (click)="onSelect(recipe)">
+            <span>{{recipe.id}}</span> - {{recipe.name}}
+          </li>
+        </ul>
+        <div *ngIf="selectedRecipe">
+          <p>{{selectedRecipe.name}} details</p>
+          <div>
+            <label>id: </label>
+            {{selectedRecipe.id}}
+          </div>
+          <div>
+            <label>name: </label>
+            <input [(ngModel)]="selectedRecipe.name" placeholder="name">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `,
+  styles: [`
+    .recipes li {
+      color: #2c3e50; }
+    .recipes li.selected:hover {
+        background-color: #2980b9 !important;
+        color: #ecf0f1; }
+    .recipes li:hover {
+        color: #7f8c8d;
+        left: .1em; }
+  `]
 })
 
 export class AppComponent {
-
+  title = 'List of Recipes';
+  recipes = RECIPES;
+  selectedRecipe: Recipe;
+  onSelect(recipe: Recipe): void {
+    this.selectedRecipe = recipe;
+  }
 }
+
+export class Recipe {
+  id: number;
+  name: string;
+}
+
+const RECIPES: Recipe[] = [
+  { id: 2, name: 'Zucchini Enchiladas'},
+  { id: 3, name: 'Chicken Zucchini Alfredo'},
+  { id: 4, name: 'Cheesesteak Stuffed Peppers'},
+  { id: 5, name: 'Mini Pepper Pizzas'},
+  { id: 6, name: 'Tuscan Spaghetti Squash'},
+  { id: 7, name: 'Zucchini Taco Roll-Ups'}
+];
---
+++ app/recipe-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'recipe-detail',
+})
+
+export class RecipeDetailComponent {
+  
+}
