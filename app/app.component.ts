6f205702feb297f6bb102d0f09d0329dd4e42e7f
Guest User <Guest@epicodus-5.local>
2017-10-24 19:32:50 -0700

    .

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <my-recipes></my-recipes>
+    <a routerLink="/recipes">Recipes</a>
+    <router-outlet></router-outlet>
   `
 })
 export class AppComponent {
